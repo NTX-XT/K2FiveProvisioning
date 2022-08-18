aac716f92278f25c6c1f2f16a24f551c5dcbedb4
John Pouliezos <jpouliezos@gmail.com>
2022-08-18 14:04:33 +0300

    Documenting Provisioning steps

--- Provision-AzEnvironment.ps1
+++ Provision-AzEnvironment.ps1
@@ -1,3 +1,9 @@
 # Script that will provision the K2 Five Azure Env
 Import-Module Az -Force
 Connect-AzAccount -Subscription sub_immersion
+
+
+# 1. Create the resource group
+# 2. Provision Azure SQL DB
+# 3. Create networking (vNet)
+# 4. Create the VM
